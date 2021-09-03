35140fcf1f613f359c616aed7b952479aa16a85a
Darek7D <darek7d@gmail.com>
2021-09-03 12:15:53 +0200

    Circle element - initial

---
+++ src/SvgBlazor/Elements/Circle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace SvgBlazor.Elements
+{
+    public class Circle : ComponentBase
+    {
+        [Parameter]
+        public double Radius { get; set; }
+
+        [Parameter]
+        public double CenterX { get; set; }
+
+        [Parameter]
+        public double CenterY { get; set; }
+
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            base.BuildRenderTree(builder);
+
+            builder.OpenElement(0, "circle");
+            builder.AddAttribute(1, "cx", CenterX);
+            builder.AddAttribute(2, "cy", CenterY);
+            builder.AddAttribute(2, "r", Radius);
+            builder.CloseComponent();
+        }
+    }
+}
