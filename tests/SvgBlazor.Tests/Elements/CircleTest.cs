0c63bcd7dd53fc2110d96caf6836a46893f4fd91
Darek7D <darek7d@gmail.com>
2021-09-06 19:59:13 +0200

    Circle test

---
+++ tests/SvgBlazor.Tests/Elements/CircleTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xunit;
+using SvgBlazor.Elements;
+using Bunit;
+
+namespace SvgBlazor.Tests
+{
+    public class CircleTest : TestContext
+    {
+        [Fact]
+        public void RendersSvgCircle_GivenSvgBlazorCircle()
+        {
+            var comp = RenderComponent<Circle>(parameters => parameters
+                .Add(p => p.CenterX, 1)
+                .Add(p => p.CenterY, 2)
+                .Add(p => p.Radius, 3)
+            );
+
+            Assert.StartsWith("<circle", comp.Markup.Trim());
+            Assert.Contains("cx=\"1\"", comp.Markup);
+            Assert.Contains("cy=\"2\"", comp.Markup);
+            Assert.Contains("r=\"3\"", comp.Markup);
+        }
+
+        [Fact]
+        public void RendersSvgCircleWithoutParameters_GivenSvgBlazorCircleWithoutParameters()
+        {
+            var comp = RenderComponent<Circle>();
+
+            Assert.Equal("<circle></circle>", comp.Markup.Trim());
+        }
+    }
+}
