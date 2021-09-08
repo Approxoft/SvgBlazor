7a416526092390662ad5e68191ee31b125ee5490
Aro Wierzbowski <arowierzbowski@gmail.com>
2021-09-08 14:16:16 +0200

    Added rect element

---
+++ src/SvgBlazor/Elements/SvgRect.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace SvgBlazor
+{
+    /// <summary>
+    /// SVG rect element.
+    /// </summary>
+    public class SvgRect : ComponentBase
+    {
+        /// <summary>
+        /// The x coordinate of the rect. Defaults to 0.
+        /// </summary>
+        [Parameter] public SvgValue X { get; set; }
+
+        /// <summary>
+        /// The y coordinate of the rect. Defaults to 0.
+        /// </summary>
+        [Parameter] public SvgValue Y { get; set; }
+
+        /// <summary>
+        /// The width of the rect.
+        /// </summary>
+        [Parameter] public SvgValue Width { get; set; }
+
+        /// <summary>
+        /// The height of the rect.
+        /// </summary>
+        [Parameter] public SvgValue Height { get; set; }
+
+        /// <summary>
+        /// The horizontal corner radius of the rect. Defaults to Ry (if specified).
+        /// </summary>
+        [Parameter] public SvgValue Rx { get; set; }
+
+        /// <summary>
+        /// The vertical corner radius of the rect. Defaults to Rx (if specified).
+        /// </summary>
+        [Parameter] public SvgValue Ry { get; set; }
+
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            base.BuildRenderTree(builder);
+
+            builder.OpenElement(0, "rect");
+            builder.AddAttribute(1, "x", X);
+            builder.AddAttribute(2, "y", Y);
+            builder.AddAttribute(3, "width", Width);
+            builder.AddAttribute(4, "height", Height);
+            builder.AddAttribute(5, "rx", Rx);
+            builder.AddAttribute(6, "ry", Ry);
+            builder.CloseComponent();
+        }
+    }
+}
---
+++ tests/SvgBlazor.Tests/Elements/RectTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Xunit;
+using Bunit;
+
+namespace SvgBlazor.Tests
+{
+    public class RectTest : TestContext
+    {
+        [Fact]
+        public void RendersSvgRectWithParameters()
+        {
+            var comp = RenderComponent<SvgRect>(parameters => parameters
+                .Add(p => p.X, 1)
+                .Add(p => p.Y, 2)
+                .Add(p => p.Width, 3)
+                .Add(p => p.Height, 4)
+                .Add(p => p.Rx, 5)
+                .Add(p => p.Ry, 6)
+            );
+
+            Assert.StartsWith("<rect", comp.Markup.Trim());
+            Assert.Contains("x=\"1\"", comp.Markup);
+            Assert.Contains("y=\"2\"", comp.Markup);
+            Assert.Contains("width=\"3\"", comp.Markup);
+            Assert.Contains("height=\"4\"", comp.Markup);
+            Assert.Contains("rx=\"5\"", comp.Markup);
+            Assert.Contains("ry=\"6\"", comp.Markup);
+        }
+
+        [Fact]
+        public void RendersSvgRectWithoutParameters()
+        {
+            var comp = RenderComponent<SvgRect>();
+
+            Assert.Equal("<rect></rect>", comp.Markup.Trim());
+        }
+    }
+}
