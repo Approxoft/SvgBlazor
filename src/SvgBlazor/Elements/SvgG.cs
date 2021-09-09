2c3161bf4f172b08aac92576c9f291b5d523c7ec
Aro W <reak45@gmail.com>
2021-09-09 13:06:24 +0200

    Add SVG g element (#27)
    Closes #26 
    Co-authored-by: Aro Wierzbowski <arowierzbowski@gmail.com>

---
+++ src/SvgBlazor/Elements/SvgG.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace SvgBlazor
+{
+    /// <summary>
+    /// Svg G element
+    /// </summary>
+    public class SvgG : ComponentBase
+    {
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            base.BuildRenderTree(builder);
+
+            builder.OpenElement(0, "g");
+            builder.AddContent(1, ChildContent);
+            builder.CloseComponent();
+        }
+    }
+}
---
+++ tests/SvgBlazor.Tests/Elements/SvgGTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Xunit;
+using Bunit;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace SvgBlazor.Tests
+{
+    public class SvgGTest : TestContext
+    {
+        [Fact]
+        public void RendersSvgGWithChildContent()
+        {
+            var comp = RenderComponent<SvgG>(parameters => parameters
+                .AddChildContent<ChildComponent>());
+
+            Assert.StartsWith("<g", comp.Markup.Trim());
+            Assert.Contains("<child", comp.Markup);
+        }
+
+        [Fact]
+        public void RendersSvgGWithoutParameters()
+        {
+            var comp = RenderComponent<SvgG>();
+
+            Assert.Equal("<g></g>", comp.Markup.Trim());
+        }
+
+        private class ChildComponent : ComponentBase
+        {
+            protected override void BuildRenderTree(RenderTreeBuilder builder)
+            {
+                base.BuildRenderTree(builder);
+
+                builder.OpenElement(0, "child");
+                builder.CloseComponent();
+            }
+        }
+    }
+}
