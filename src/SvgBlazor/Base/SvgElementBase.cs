a500ba39b281b052518f1c0311dd90c08727eff2
Darek7D <darek7d@gmail.com>
2021-09-16 12:24:02 +0200

    Base class for all SVG elements

---
+++ src/SvgBlazor/Base/SvgElementBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using System.Collections.Generic;
+
+namespace SvgBlazor
+{
+    public class SvgElementBase : ComponentBase
+    {
+        [Parameter] public SvgValue Id { get; set; }
+
+        [Parameter] public string Class { get; set; }
+
+        [Parameter] public string Style { get; set; }
+    }
+}
