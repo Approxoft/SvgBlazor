847e1a3ee962c26b35bd53ff69a69fe44cb51c07
Darek7D <darek7d@gmail.com>
2021-09-18 11:59:22 +0200

    Add SVG line element (#40)

---
+++ tests/SvgBlazor.Tests/Elements/LineTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xunit;
+using Bunit;
+
+namespace SvgBlazor.Tests
+{
+    public class LineTest : TestContext
+    {
+        [Fact]
+        public void RendersSvgCircleWithParameters()
+        {
+            var comp = RenderComponent<SvgLine>(parameters => parameters
+                .Add(p => p.X1, 1)
+                .Add(p => p.Y1, 2)
+                .Add(p => p.X2, 3)
+                .Add(p => p.Y2, 4)
+            );
+
+            Assert.StartsWith("<line", comp.Markup.Trim());
+            Assert.Contains("x1=\"1\"", comp.Markup);
+            Assert.Contains("y1=\"2\"", comp.Markup);
+            Assert.Contains("x2=\"3\"", comp.Markup);
+            Assert.Contains("y2=\"4\"", comp.Markup);
+        }
+
+        [Fact]
+        public void RendersSvgCircleWithoutParameters()
+        {
+            var comp = RenderComponent<SvgLine>();
+
+            Assert.Equal("<line></line>", comp.Markup.Trim());
+        }
+    }
+}
