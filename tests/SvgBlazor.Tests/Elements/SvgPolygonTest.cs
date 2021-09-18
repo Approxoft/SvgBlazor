801c6733602f3c19952a08ddd06851781507529f
Aro W <reak45@gmail.com>
2021-09-18 20:25:48 +0200

    Add SVG polygon element (#43)
    Closes #35 
    Co-authored-by: Aro Wierzbowski <arowierzbowski@gmail.com>

---
+++ tests/SvgBlazor.Tests/Elements/SvgPolygonTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xunit;
+using Bunit;
+
+namespace SvgBlazor.Tests
+{
+    public class SvgPolygonTest : TestContext
+    {
+        [Fact]
+        public void RendersSvgPolygonWithParameters()
+        {
+            var comp = RenderComponent<SvgPolygon>(parameters => parameters
+                .Add(p => p.Points, "0 0 200 200")
+            );
+
+            Assert.StartsWith("<polygon", comp.Markup.Trim());
+            Assert.Contains("points=\"0 0 200 200\"", comp.Markup);
+        }
+
+        [Fact]
+        public void RendersSvgPolygonWithoutParameters()
+        {
+            var comp = RenderComponent<SvgPolygon>();
+
+            Assert.Equal("<polygon></polygon>", comp.Markup.Trim());
+        }
+    }
+}
