2c3b4dd4f91d3bb3bfd6531a390b2c64b5deb1fe
Darek7D <darek7d@gmail.com>
2021-09-18 09:09:57 +0200

    Line element

---
+++ src/SvgBlazor/Elements/SvgLine.razor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace SvgBlazor
+{
+    /// <summary>
+    /// SVG circle element.
+    /// </summary>
+    public partial class SvgLine : SvgElementBase
+    {
+        /// <summary>
+        /// Start point x-axis coordinate.
+        /// </summary>
+        [Parameter] public SvgValue X1 { get; set; }
+
+        /// <summary>
+        /// Start point y-axis coordinate.
+        /// </summary>
+        [Parameter] public SvgValue Y1 { get; set; }
+
+        /// <summary>
+        /// End point x-axis coordinate.
+        /// </summary>
+        [Parameter] public SvgValue X2 { get; set; }
+
+        /// <summary>
+        /// End point y-axis coordinate.
+        /// </summary>
+        [Parameter] public SvgValue Y2 { get; set; }
+    }
+}
