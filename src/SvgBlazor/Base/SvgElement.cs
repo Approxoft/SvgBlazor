8c7e373a02d7c2a18eb2fde6edeb64b6afc1114e
Darek7D <darek7d@gmail.com>
2021-09-19 22:52:19 +0200

    Draggable test

--- src/SvgBlazor/Base/SvgElement.cs
+++ src/SvgBlazor/Base/SvgElement.cs
@@ -16,6 +16,11 @@ namespace SvgBlazor
         public string Style { get; set; }
 
         public EventCallback<MouseEventArgs> OnClick { get; set; }
+        public EventCallback<MouseEventArgs> OnMouseDown { get; set; }
+        public EventCallback<MouseEventArgs> OnMouseMove { get; set; }
+        public EventCallback<MouseEventArgs> OnMouseUp { get; set; }
+
+        private ISvgContainer _parent;
 
         public abstract string Tag();
 
@@ -36,13 +41,55 @@ namespace SvgBlazor
             builder.AddAttribute(1, "id", Id);
             builder.AddAttribute(2, "class", Class);
             builder.AddAttribute(3, "style", Style);
-            var onClickHandler = EventCallback.Factory.Create<MouseEventArgs>(this, OnClickHandler);
-            builder.AddAttribute(4, "onclick", onClickHandler);
+            var onMouseOverHandler = EventCallback.Factory.Create<MouseEventArgs>(this, OnMouseOverHandler);
+            builder.AddAttribute(4, "onmouseover", onMouseOverHandler);
+            var onMouseOutHandler = EventCallback.Factory.Create<MouseEventArgs>(this, OnMouseOutHandler);
+            builder.AddAttribute(5, "onmouseout", onMouseOutHandler);
         }
 
-        public void OnClickHandler(MouseEventArgs args)
+        public virtual void OnMouseOverHandler(MouseEventArgs args)
+        {
+            _parent.ElementMouseOver(this, args);
+        }
+
+        public virtual void OnMouseOutHandler(MouseEventArgs args)
+        {
+            _parent.ElementMouseOut(this, args);
+        }
+
+        public virtual void OnClickHandler(MouseEventArgs args)
         {
             OnClick.InvokeAsync(args);
+        }
+
+        public virtual void OnMouseDownHandler(MouseEventArgs args)
+        {            
+            OnMouseDown.InvokeAsync(args);
+        }
+
+        public virtual void OnMouseMoveHandler(MouseEventArgs args)
+        {
+            OnMouseMove.InvokeAsync(args);
+        }
+
+        public virtual void OnMouseUpHandler(MouseEventArgs args)
+        {
+            OnMouseUp.InvokeAsync(args);
+        }
+
+        public virtual void SetParent(ISvgContainer svgContainer)
+        {
+            _parent = svgContainer;
+        }
+
+        public virtual ISvgContainer Parent()
+        {
+            return _parent;
+        }
+
+        public virtual void Refresh()
+        {
+            _parent.Refresh();
         }
     }
 }
--- src/SvgBlazor/Interfaces/ISvgContainer.cs
+++ src/SvgBlazor/Interfaces/ISvgContainer.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using Microsoft.AspNetCore.Components.Web;
+
 namespace SvgBlazor.Interfaces
 {
     public interface ISvgContainer
     {
         ISvgContainer Add(SvgElement element);
         ISvgContainer Remove(SvgElement element);
+        void Refresh();
+        void ElementMouseOver(SvgElement element, MouseEventArgs args);
+        void ElementMouseOut(SvgElement element, MouseEventArgs args);
     }
 }
