695eb5aaabdf4793a5123074ee320316c8034a9e
Darek7D <darek7d@gmail.com>
2021-09-22 20:22:45 +0200

    Update polygons to dynamic elements (#46)

---
+++ src/SvgBlazor/Elements/SvgPolygon.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace SvgBlazor
+{
+    /// <summary>
+    /// SVG polygon element.
+    /// </summary>
+    public class SvgPolygon : SvgElement
+    {
+        /// <summary>
+        /// The list of points (pairs of x,y absolute coordinates) required to draw the polygon.
+        /// </summary>
+        public string Points { get; set; }
+
+        /// <summary>
+        /// The color used to paint the outline of the shape.
+        /// </summary>
+        public string Stroke { get; set; }
+
+        public override string Tag() => "polygon";
+
+        public override void AddAttributes(RenderTreeBuilder builder)
+        {
+            base.AddAttributes(builder);
+            builder.AddAttribute(0, "points", Points);
+            builder.AddAttribute(1, "stroke", Stroke);
+        }
+    }
+}
