571ca5e554e85c9534a85e77c608bc12b8ec83f7
Darek7D <darek7d@gmail.com>
2021-09-23 00:08:10 +0200

    Add SVG ellipse element

---
+++ src/SvgBlazor.Docs/Pages/Elements/Ellipse/Examples/EllipseExample.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using SvgBlazor.Docs.Interfaces;
+
+namespace SvgBlazor.Docs.Examples
+{
+    public class EllipseExample: IExampleCode
+    {
+        public void Example(SvgComponent svg)
+        {
+            var ellipse = new SvgEllipse()
+            {
+                CenterX = 100,
+                CenterY = 100,
+                RadiusX = 40,
+                RadiusY = 20
+            };
+            svg.Add(ellipse);
+        }
+    }
+}
---
+++ src/SvgBlazor/Elements/SvgEllipse.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace SvgBlazor
+{
+    /// <summary>
+    /// SVG circle element.
+    /// </summary>
+    public class SvgEllipse : SvgElement
+    {
+        /// <summary>
+        /// The x-axis coordinate of the center of the circle.
+        /// </summary>
+        public SvgValue CenterX { get; set; }
+
+        /// <summary>
+        /// The y-axis coordinate of the center of the circle.
+        /// </summary>
+        public SvgValue CenterY { get; set; }
+
+        /// <summary>
+        /// The radius on the x axis.
+        /// </summary>
+        public SvgValue RadiusX { get; set; }
+
+        /// <summary>
+        /// The radius on the y axis.
+        /// </summary>
+        public SvgValue RadiusY { get; set; }
+
+        public override string Tag() => "ellipse";
+
+        public override void AddAttributes(RenderTreeBuilder builder)
+        {
+            base.AddAttributes(builder);
+            builder.AddAttribute(0, "cx", CenterX);
+            builder.AddAttribute(1, "cy", CenterY);
+            builder.AddAttribute(2, "rx", RadiusX);
+            builder.AddAttribute(3, "ry", RadiusY);
+        }
+    }
+}
