9189d4f9e8ab780708109744ff5f3dd6d3357e8d
Darek7D <darek7d@gmail.com>
2021-09-22 20:09:11 +0200

    Update polygons to dynamic elements

---
+++ src/SvgBlazor.Docs/Pages/Elements/Polygon/Examples/PolygonExample.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using SvgBlazor.Docs.Interfaces;
+
+namespace SvgBlazor.Docs.Examples
+{
+    public class PolygonExample : IExampleCode
+    {
+        public void Example(SvgComponent svg)
+        {
+            var polygon = new SvgPolygon()
+            {
+                Points = "0 0 50 50 100 50"
+            };
+            svg.Add(polygon);
+        }
+    }
+}
