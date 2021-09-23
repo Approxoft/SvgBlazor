d81155cac6ebf75b7a4d151ca991ce89fcddc05d
Darek7D <darek7d@gmail.com>
2021-09-23 22:53:18 +0200

    Tests

---
+++ tests/SvgBlazor.Tests/Elements/SvgComponent.cs
@@ -0,0 +1,81 @@
+﻿using Xunit;
+using Bunit;
+
+namespace SvgBlazor.Tests
+{
+    public class SvgComponentTest : TestContext
+    {
+        [Fact]
+        public void SvgComponentOnClick()
+        {
+            bool eventHandled = false;
+
+            var comp = RenderComponent<SvgComponent>(parameters => parameters
+                .Add(p => p.OnClick, _ => { eventHandled = true; })
+            );
+
+            comp.Render();
+
+            var svg = comp.Find("svg");
+            svg.Click();
+
+            Assert.True(eventHandled);
+        }
+
+        [Fact]
+        public void SvgComponentOnMouseDown()
+        {
+            bool eventHandled = false;
+
+            var comp = RenderComponent<SvgComponent>(parameters => parameters
+                .Add(p => p.OnMouseDown, _ => { eventHandled = true; })
+            );
+
+            comp.Render();
+
+            var svg = comp.Find("svg");
+            svg.MouseDown();
+
+            Assert.True(eventHandled);
+        }
+
+        [Fact]
+        public void SvgComponentOnMouseUp()
+        {
+            bool eventHandled = false;
+
+            var comp = RenderComponent<SvgComponent>(parameters => parameters
+                .Add(p => p.OnMouseUp, _ => { eventHandled = true; })
+            );
+
+            comp.Render();
+
+            var svg = comp.Find("svg");
+            svg.MouseUp();
+
+            Assert.True(eventHandled);
+        }
+
+        [Fact]
+        public void SvgComponentOnMouseMove()
+        {
+            bool eventHandled = false;
+
+            var comp = RenderComponent<SvgComponent>(parameters => parameters
+                .Add(p => p.OnMouseMove, _ => { eventHandled = true; })
+            );
+
+            comp.Render();
+
+            var svg = comp.Find("svg");
+            svg.MouseMove();
+
+            Assert.True(eventHandled);
+        }
+
+        class DummyElement : SvgElement
+        {
+            public override string Tag() => "dummy";
+        }
+    }
+}
