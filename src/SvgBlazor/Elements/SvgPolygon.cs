5764c5ad93a5e283d5d397af74cfa8b55f1d701a
Darek7D <darek7d@gmail.com>
2021-09-30 23:42:18 +0200

    Polygon bounding rect

--- src/SvgBlazor/Elements/SvgPolygon.cs
+++ src/SvgBlazor/Elements/SvgPolygon.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
+using System.Text;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Rendering;
 
@@ -11,24 +13,83 @@ namespace SvgBlazor
     public class SvgPolygon : SvgElement
     {
         /// <summary>
-        /// The list of points (pairs of x,y absolute coordinates) required to draw the polygon.
-        /// </summary>
-        public string Points { get; set; }
-
-        /// <summary>
         /// The color used to paint the outline of the shape.
         /// </summary>
         public string Stroke { get; set; }
+
+        private List<PointF> _points;
+
+        private string _pointsString;
+
+        private float _boundingRectX1;
+        private float _boundingRectY1;
+        private float _boundingRectX2;
+        private float _boundingRectY2;
+        private float _boundingRectWidth;
+        private float _boundingRectHeight;
+
+        public SvgPolygon() => ResetBoundingRect();
 
         public override string Tag() => "polygon";
 
         public override void AddAttributes(RenderTreeBuilder builder)
         {
             base.AddAttributes(builder);
-            builder.AddAttribute(0, "points", Points);
+            builder.AddAttribute(0, "points", _pointsString);
             builder.AddAttribute(1, "stroke", Stroke);
         }
 
-        public override RectangleF BoundingRect() => throw new NotImplementedException();
+        public override RectangleF BoundingRect() => new(_boundingRectX1,
+                                                         _boundingRectY1,
+                                                         _boundingRectWidth,
+                                                         _boundingRectHeight);
+
+        public virtual void AddPoint(PointF point)
+        {
+            _points.Add(point);
+            UpdateBoundingRect(point);
+            PointsToString();
+        }
+
+        public virtual void ClearPoints()
+        {
+            _points.Clear();
+            ResetBoundingRect();
+            PointsToString();
+        }
+
+        private void UpdateBoundingRect(PointF point)
+        {
+            _boundingRectX1 = Math.Min(point.X, _boundingRectX1);
+            _boundingRectY1 = Math.Min(point.Y, _boundingRectY1);
+            _boundingRectX2 = Math.Min(point.X, _boundingRectX2);
+            _boundingRectY2 = Math.Min(point.Y, _boundingRectY2);
+            _boundingRectWidth = _boundingRectX2 - _boundingRectX1;
+            _boundingRectHeight = _boundingRectY2 - _boundingRectY1;
+        }
+
+        private void ResetBoundingRect()
+        {
+            _boundingRectX1 = float.MaxValue;
+            _boundingRectY1 = float.MaxValue;
+            _boundingRectX2 = float.MinValue;
+            _boundingRectY2 = float.MinValue;
+            _boundingRectWidth = 0;
+            _boundingRectHeight = 0;
+        }
+
+        private void PointsToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach(var point in _points)
+            {
+                sb.Append(point.X);
+                sb.Append(' ' );
+                sb.Append(point.Y);
+                sb.Append(' ');
+            }
+
+            _pointsString = sb.ToString();
+        }
     }
 }
