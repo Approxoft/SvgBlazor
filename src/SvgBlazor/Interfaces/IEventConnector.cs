b697abc265b03ce930132e67689e567aba82c18b
Darek7D <darek7d@gmail.com>
2021-10-02 23:59:22 +0200

    Testing

---
+++ src/SvgBlazor/Interfaces/IEventConnector.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace SvgBlazor.Interfaces
+{
+    public interface IEventConnector
+    {
+        void ElementMouseOver(ISvgElement element, MouseEventArgs args);
+        void ElementMouseOut(ISvgElement element, MouseEventArgs args);
+        Task OnClickHandler(MouseEventArgs args);
+        Task OnMouseDownHandler(MouseEventArgs args);
+        Task OnMouseMoveHandler(MouseEventArgs args);
+        Task OnMouseUpHandler(MouseEventArgs args);
+    }
+}
