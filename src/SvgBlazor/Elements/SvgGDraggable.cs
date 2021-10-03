9e00c09e8594563114d7d7a22aba64273c0610f5
Darek7D <darek7d@gmail.com>
2021-10-03 10:33:21 +0200

    Move OverElement to upper class

--- src/SvgBlazor/Elements/SvgGDraggable.cs
+++ src/SvgBlazor/Elements/SvgGDraggable.cs
@@ -17,79 +17,27 @@ namespace SvgBlazor
 
         public override RectangleF BoundingRect() => throw new NotImplementedException();
 
-        private bool _mouseDown = false;
         private float DiffX = 0;
         private float DiffY = 0;
-        private ISvgElement _overElement;
-
-        public override void ElementMouseOver(ISvgElement element, MouseEventArgs args)
-        {
-            if (_mouseDown)
-            {
-                return;
-            }
-
-            if (element != this)
-            {
-                _overElement = element;
-            }
-            base.ElementMouseOver(element, args);
-        }
-
-        public override void ElementMouseOut(ISvgElement element, MouseEventArgs args)
-        {
-            if (_mouseDown)
-            {
-                return;
-            }
-
-            if (element != this)
-            {
-                _overElement = null;
-            }
-            base.ElementMouseOut(element, args);
-        }
-
-        public override async Task OnClickHandler(MouseEventArgs args)
-        {
-            if (_overElement is not null)
-            {
-                await _overElement.OnClickHandler(args);
-                await base.OnMouseDownHandler(args);
-            }
-        }
 
         public override async Task OnMouseDownHandler(MouseEventArgs args)
         {
-            _mouseDown = true;
-            if (_overElement is not null)
+            if (OverElement is not null)
             {
-                DiffX = (float)args.OffsetX - _overElement.X;
-                DiffY = (float)args.OffsetY - _overElement.Y;
-                await _overElement.OnMouseDownHandler(args);
-                await base.OnMouseDownHandler(args);
+                DiffX = (float)args.OffsetX - OverElement.X;
+                DiffY = (float)args.OffsetY - OverElement.Y;
             }
+            await base.OnMouseDownHandler(args);
         }
 
         public override async Task OnMouseMoveHandler(MouseEventArgs args)
         {
-            if (_overElement is not null)
+            if (MouseDown && OverElement is not null)
             {
-                if (_mouseDown)
-                {
-                    _overElement.X = (float)args.OffsetX - DiffX;
-                    _overElement.Y = (float)args.OffsetY - DiffY;
-                }
-                await _overElement.OnMouseMoveHandler(args);
-                await base.OnMouseMoveHandler(args);
+                OverElement.X = (float)args.OffsetX - DiffX;
+                OverElement.Y = (float)args.OffsetY - DiffY;
             }
-        }
-
-        public override async Task OnMouseUpHandler(MouseEventArgs args)
-        {
-            _mouseDown = false;
-            await (_overElement?.OnMouseMoveHandler(args)??Task.CompletedTask);
-            await base.OnMouseUpHandler(args);
+            await base.OnMouseMoveHandler(args);
         }
     }
 }
