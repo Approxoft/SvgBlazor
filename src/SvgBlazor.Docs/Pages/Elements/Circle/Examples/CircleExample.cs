a9e91fc6569a2e550bcb7fb9cd5d81df24b0b93d
Darek7D <darek7d@gmail.com>
2021-10-06 21:09:23 +0200

    Update CircleExample.cs

--- src/SvgBlazor.Docs/Pages/Elements/Circle/Examples/CircleExample.cs
+++ src/SvgBlazor.Docs/Pages/Elements/Circle/Examples/CircleExample.cs
@@ -12,7 +12,7 @@ namespace SvgBlazor.Docs.Examples
                 CenterX = 100,
                 CenterY = 100,
                 Radius = 20,
-                Fill = new SvgFill { Color="blue", Opacity = 0.5f },
+                Fill = new SvgFill { Color = "blue", Opacity = 0.5f },
             };
             svg.Add(circle);
         }
