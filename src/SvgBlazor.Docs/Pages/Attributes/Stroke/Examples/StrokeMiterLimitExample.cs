7bc3178d3f6b0159780fc252ca023f9bc975494c
Aro W <reak45@gmail.com>
2021-10-08 15:10:49 +0200

    Provide StrokeMiterLimit example when Path is implemented (#81)
    Co-authored-by: Aro Wierzbowski <arowierzbowski@gmail.com>

---
+++ src/SvgBlazor.Docs/Pages/Attributes/Stroke/Examples/StrokeMiterLimitExample.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using SvgBlazor.Docs.Interfaces;
+using SvgBlazor.Elements;
+
+namespace SvgBlazor.Docs.Examples
+{
+    public class StrokeMiterLimitExample : IExampleCode
+    {
+        private int? _miterLimit;
+
+        public StrokeMiterLimitExample(int? miterLimit = null) => _miterLimit = miterLimit;
+
+        public void Example(SvgComponent svg)
+        {
+            var path = new SvgPath
+            {
+                Path = "m3,85 c5,-3 90,-73 90,-73 c0,0 100,75 100,75",
+                Stroke = new SvgStroke
+                {
+                    Color = "black",
+                    LineJoin = StrokeLineJoinStyle.Miter,
+                    MiterLimit = _miterLimit,
+                    Width = 15,
+                },
+                Fill = new SvgFill
+                {
+                    Color = "none",
+                },
+            };
+
+            svg.Add(path);
+        }
+    }
+}
