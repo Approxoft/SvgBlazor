2a610211cd15fbb07d88531546d4c5e47836bf31
Aro Wierzbowski <arowierzbowski@gmail.com>
2021-10-08 13:07:20 +0200

    Add SVG path element

---
+++ tests/SvgBlazor.Tests/Elements/SvgPathTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Bunit;
+using SvgBlazor.Elements;
+using Xunit;
+
+namespace SvgBlazor.Tests.Elements
+{
+    public class SvgPathTest : TestContext
+    {
+        [Fact]
+        public void RendersAttributes()
+        {
+            var comp = RenderComponent<SvgComponent>();
+
+            comp.InvokeAsync(() => comp.Instance.Add(new SvgPath
+            {
+                Path = "M10 10",
+            }));
+
+            comp.Render();
+
+            var element = comp.Find("path");
+            Assert.Contains("M10 10", element.GetAttribute("d"));
+        }
+    }
+}
