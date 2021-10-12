d23f9ce175b2054c7379b12a3fba3546c844f9a5
Aro Wierzbowski <arowierzbowski@gmail.com>
2021-10-12 11:11:52 +0200

    Improve enums

--- src/SvgBlazor/Enums/FillRule.cs
+++ src/SvgBlazor/Enums/FillRule.cs
@@ -1,8 +1,19 @@
+using System.ComponentModel;
+
 namespace SvgBlazor
 {
     public enum FillRule
     {
+        /// <summary>
+        /// Non zero fill rule.
+        /// </summary>
+        [Description("nonzero")]
         NonZero,
+
+        /// <summary>
+        /// Even odd fill rule.
+        /// </summary>
+        [Description("evenodd")]
         EvenOdd,
     }
 }
