46d9fb421ac50896ae57148fc3a03001cff8d03f
Darek7D <darek7d@gmail.com>
2021-10-21 21:22:00 +0200

    Source code component (#94)

--- src/SvgBlazor.Docs.Generator/Program.cs
+++ src/SvgBlazor.Docs.Generator/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO;
 
 namespace SvgBlazor.Docs.Generator
 {
     public class Program
     {
-        public static int Main()
+        public static int Main(string[] args)
         {
+            if (args.Length == 1)
+            {
+                ProjectPaths.SourcePath = Path.Combine(args[0], "src");
+            }
+            else
+            {
+                Console.WriteLine($"SvgBlazor.Docs.Generator failed: no solution path provided!");
+                return 1;
+            }
+
             try
             {
                 var stopWatch = Stopwatch.StartNew();
