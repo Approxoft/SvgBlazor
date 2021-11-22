668b8cb07c4c14e5e6ea90fb749394668c23438e
Darek7D <darek7d@gmail.com>
2021-11-22 12:12:40 +0100

    Api component (#104)

---
+++ src/SvgBlazor.Docs/Models/ElementApiMethod.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SvgBlazor.Docs.Models
+{
+    public class ElementApiMethod
+    {
+        public string Name { get; set; }
+
+        public string ReturnValue { get; set; }
+
+        public IEnumerable<string> Parameters { get; set; }
+
+        public string Description { get; set; }
+    }
+}
---
+++ src/SvgBlazor.Docs/Models/ElementApiProperty.cs
@@ -0,0 +1,13 @@
+namespace SvgBlazor.Docs.Models
+{
+    public class ElementApiProperty
+    {
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+
+        public string DeclaringType { get; set; }
+
+        public string Description { get; set; }
+    }
+}
