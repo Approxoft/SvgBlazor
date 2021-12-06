97a7efcfa61252f8b5d7a055cc0d87691b6a6946
Darek7D <darek7d@gmail.com>
2021-12-06 13:59:36 +0100

    Examples

---
+++ src/SvgBlazor.Docs/Pages/Attributes/Fill/Examples/FillOpacityExample.cs
@@ -0,0 +1,58 @@
+﻿using SvgBlazor.Docs.Interfaces;
+
+namespace SvgBlazor.Docs.Examples
+{
+    public class FillOpacityExample : IExampleCode
+    {
+        public void Example(SvgComponent svg)
+        {
+            /* #example-code-start */
+            var fill1 = new SvgFill
+            {
+                Color = "black",
+                Opacity = 0.3f,
+            };
+
+            var fill2 = new SvgFill
+            {
+                Color = "black",
+                Opacity = 0.6f,
+            };
+
+            var fill3 = new SvgFill
+            {
+                Color = "black",
+                Opacity = 1f,
+            };
+            /* #example-code-end */
+
+            var circle1 = new SvgCircle
+            {
+                CenterX = 100,
+                CenterY = 100,
+                Radius = 75,
+                Fill = fill1,
+            };
+
+            var circle2 = new SvgCircle
+            {
+                CenterX = 200,
+                CenterY = 100,
+                Radius = 75,
+                Fill = fill2,
+            };
+
+            var circle3 = new SvgCircle
+            {
+                CenterX = 300,
+                CenterY = 100,
+                Radius = 75,
+                Fill = fill3,
+            };
+
+            svg.Add(circle1);
+            svg.Add(circle2);
+            svg.Add(circle3);
+        }
+    }
+}
---
+++ src/SvgBlazor.Docs/Pages/Attributes/Fill/Examples/FillRuleEvenOddExample.cs
@@ -0,0 +1,30 @@
+﻿using SvgBlazor.Docs.Interfaces;
+using SvgBlazor.Elements;
+
+namespace SvgBlazor.Docs.Examples
+{
+    public class FillRuleEvenOddExample : IExampleCode
+    {
+        public void Example(SvgComponent svg)
+        {
+            /* #example-code-start */
+            var shape1 = new SvgPath
+            {
+                Path = "M 20 180 H 180 L 100 20 Z M 80 148 H 120 L 100 108 Z",
+                Fill = new SvgFill { Rule = FillRule.EvenOdd },
+                Stroke = new SvgStroke { Color = "grey" },
+            };
+
+            var shape2 = new SvgPath
+            {
+                Path = "M 220 180 H 380 L 300 20 z M 320 147 H 280 L 300 108 Z",
+                Fill = new SvgFill { Rule = FillRule.EvenOdd },
+                Stroke = new SvgStroke { Color = "grey" },
+            };
+            /* #example-code-end */
+
+            svg.Add(shape1);
+            svg.Add(shape2);
+        }
+    }
+}
