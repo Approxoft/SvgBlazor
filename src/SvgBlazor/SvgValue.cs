7c1e86779d929b95dd8807e07e8bb5ca03259926
Darek7D <darek7d@gmail.com>
2022-01-08 15:39:14 +0100

    Update SvgValue.cs

--- src/SvgBlazor/SvgValue.cs
+++ src/SvgBlazor/SvgValue.cs
@@ -72,20 +72,47 @@ namespace SvgBlazor
         }
 
         /// <summary>
-        /// 
+        /// Gets the keyword for 'auto' value.
         /// </summary>
         public static string Auto { get; } = "auto";
 
+        /// <summary>
+        /// Conversion operator from int type.
+        /// </summary>
+        /// <param name="value">An int value.</param>
         public static implicit operator SvgValue(int value) => new SvgValue(value);
 
+        /// <summary>
+        /// Conversion operator from float type.
+        /// </summary>
+        /// <param name="value">A float value.</param>
         public static implicit operator SvgValue(float value) => new SvgValue(value);
 
+        /// <summary>
+        /// Conversion operator from string type.
+        /// </summary>
+        /// <param name="value">A string value.</param>
         public static implicit operator SvgValue(string value) => new SvgValue(value);
 
+        /// <summary>
+        /// Conversion operator to float type.
+        /// </summary>
+        /// <param name="d">A SvgValue.</param>
         public static implicit operator float(SvgValue d) => d.ToFloat();
 
+        /// <summary>
+        /// Returns a float that represents the current object.
+        /// </summary>
+        /// <returns>The float value.</returns>
         public float ToFloat() => _floatValue;
 
+        /// <summary>
+        /// Returns a string that represents the current object.
+        /// </summary>
+        /// <remarks>
+        /// If SvgValue stores a float value then the configured unit is added at the end.
+        /// </remarks>
+        /// <returns>A string value.</returns>
         public override string ToString()
         {
             switch (_valueType)
