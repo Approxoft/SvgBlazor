2406c151a6b33470684191f177f331ce9bad6b63
Aro Wierzbowski <arowierzbowski@gmail.com>
2022-01-12 13:00:36 +0100

    Fixes

---
+++ src/SvgBlazor.Docs/Pages/Elements/SvgComponent/Examples/SvgComponentRemoveExample.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using SvgBlazor.Docs.Interfaces;
+
+namespace SvgBlazor.Docs.Examples
+{
+    public class SvgComponentRemoveExample : IExampleCode
+    {
+        public void Example(SvgComponent svg)
+        {
+            /* #example-code-start */
+            var text = new SvgText
+            {
+                X = 25,
+                Y = 25,
+                Text = "Click to remove",
+            };
+
+            text.OnClick = EventCallback.Factory.Create<MouseEventArgs>(text, (args) =>
+            {
+                svg.Remove(text);
+            });
+
+            svg.Add(text);
+            /* #example-code-end */
+        }
+    }
+}
