6df92fad3a49a237781e7c9e564856a6594fae10
Aro Wierzbowski <arowierzbowski@gmail.com>
2022-01-12 16:09:30 +0100

    More fixes

---
+++ src/SvgBlazor.Docs/wwwroot/js/SvgBlazor.Docs.js
@@ -0,0 +1,16 @@
+﻿window.highlightCodeElement = (element) => {
+    if (element && element.children.length > 0) {
+        var firstChild = element.children[0];
+
+        if (firstChild.children.length > 0) {
+            var firstGrandchild = firstChild.children[0];
+            hljs.highlightElement(firstGrandchild);
+        } else {
+            hljs.highlightElement(firstChild);
+        }
+    }
+};
+
+window.highlightAllCode = () => {
+    hljs.highlightAll();
+};
