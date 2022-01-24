1e8e7c7bb0c17e3e7cf1ef1b85ea797e0bd6636a
Darek7D <darek7d@gmail.com>
2022-01-24 18:03:44 +0100

    In progress

--- tests/SvgBlazor.Tests/Attributes/StrokeAttributeTest.cs
+++ tests/SvgBlazor.Tests/Attributes/StrokeAttributeTest.cs
@@ -1,4 +1,7 @@
-﻿using System.Drawing;
+﻿// Copyright (c) Approxoft. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System.Drawing;
 using Microsoft.AspNetCore.Components.Rendering;
 using Xunit;
 using Bunit;
--- tests/SvgBlazor.Tests/Elements/SvgTextTest.cs
+++ tests/SvgBlazor.Tests/Elements/SvgTextTest.cs
@@ -1,4 +1,7 @@
-﻿using Xunit;
+﻿// Copyright (c) Approxoft. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using Xunit;
 using Bunit;
 
 namespace SvgBlazor.Tests
