8dd308f1dab04c9f7f432dc84b8d29ef3771d10e
Darek7D <darek7d@gmail.com>
2022-01-26 19:35:33 +0000

    Deploying to gh-pages from  @ e2d155c199c3c58f107a8acbba6b950d847b3d6b 🚀

---
+++ js/SvgBlazor.Docs.js
@@ -0,0 +1,16 @@
+﻿window.highlightCodeElement = (element) => {
+    if (element && element.children.length > 0) {
+        var firstChild = element.firstElementChild;
+
+        if (firstChild.children.length > 0) {
+            var firstGrandchild = firstChild.firstElementChild;
+            hljs.highlightElement(firstGrandchild);
+        } else {
+            hljs.highlightElement(firstChild);
+        }
+    }
+};
+
+window.highlightAllCode = () => {
+    hljs.highlightAll();
+};
