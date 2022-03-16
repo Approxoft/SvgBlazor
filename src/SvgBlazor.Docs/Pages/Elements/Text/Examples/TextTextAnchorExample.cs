c020ad3187edcd0a85d1c14a18f1932483b81ddf
Aro Wierzbowski <arowierzbowski@gmail.com>
2022-03-16 15:20:58 +0100

    Implement text-anchor in SvgText

---
+++ src/SvgBlazor.Docs/Pages/Elements/Text/Examples/TextTextAnchorExample.cs
@@ -0,0 +1,52 @@
+﻿// Copyright (c) Approxoft. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using SvgBlazor.Docs.Interfaces;
+
+namespace SvgBlazor.Docs.Examples
+{
+    public class TextTextAnchorExample : IExampleCode
+    {
+        public void Example(SvgComponent svg)
+        {
+            {
+                /* #example-code-start */
+                var text = new SvgText
+                {
+                    X = 200,
+                    Y = 30,
+                    Text = "Let's drop the anchor",
+                    TextAnchor = TextAnchor.Start,
+                };
+                /* #example-code-end */
+                svg.Add(text);
+            }
+
+            {
+                /* #example-code-start */
+                var text = new SvgText
+                {
+                    X = 200,
+                    Y = 60,
+                    Text = "Let's drop the anchor",
+                    TextAnchor = TextAnchor.Middle,
+                };
+                /* #example-code-end */
+                svg.Add(text);
+            }
+
+            {
+                /* #example-code-start */
+                var text = new SvgText
+                {
+                    X = 200,
+                    Y = 90,
+                    Text = "Let's drop the anchor",
+                    TextAnchor = TextAnchor.End,
+                };
+                /* #example-code-end */
+                svg.Add(text);
+            }
+        }
+    }
+}
---
+++ src/SvgBlazor/Enums/TextAnchor.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Approxoft. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System.ComponentModel;
+
+namespace SvgBlazor
+{
+    /// <summary>
+    /// The anchor of the text.
+    /// </summary>
+    public enum TextAnchor
+    {
+        /// <summary>
+        /// The given x and y coordinates will become the start location of the text.
+        /// </summary>
+        [Description("start")]
+        Start,
+
+        /// <summary>
+        /// The given x and y coordinates will become the middle location of the text.
+        /// </summary>
+        [Description("middle")]
+        Middle,
+
+        /// <summary>
+        /// The given x and y coordinates will become the end location of the text.
+        /// </summary>
+        [Description("end")]
+        End,
+    }
+}
