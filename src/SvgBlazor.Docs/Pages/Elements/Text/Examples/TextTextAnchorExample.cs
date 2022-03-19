35aa6725b36f35c1960a17cfc6844484f8412a9c
Aro Wierzbowski <arowierzbowski@gmail.com>
2022-03-19 12:48:06 +0100

    Updates

--- src/SvgBlazor.Docs/Pages/Elements/Text/Examples/TextTextAnchorExample.cs
+++ src/SvgBlazor.Docs/Pages/Elements/Text/Examples/TextTextAnchorExample.cs
@@ -9,6 +9,7 @@ namespace SvgBlazor.Docs.Examples
     {
         public void Example(SvgComponent svg)
         {
+            DrawLines(svg);
             {
                 /* #example-code-start */
                 var text = new SvgText
@@ -47,6 +48,45 @@ namespace SvgBlazor.Docs.Examples
                 /* #example-code-end */
                 svg.Add(text);
             }
+
+            DrawCircles(svg);
+        }
+
+        private void DrawLines(SvgComponent svg)
+        {
+            var path = new SvgPath();
+            path.Path = "M200,0 L200,110 M0,30 L400,30 M0,60 L400,60 M0,90 L400,90";
+            path.Stroke = new SvgStroke
+            {
+                Color = "lightgray",
+                DashArray = "4",
+            };
+
+            svg.Add(path);
+        }
+
+        private void DrawCircles(SvgComponent svg)
+        {
+            var circle1 = new SvgCircle
+            {
+                CenterX = 200,
+                CenterY = 30,
+                Radius = 3,
+                Fill = new SvgFill
+                {
+                    Color = "red",
+                },
+            };
+
+            var circle2 = new SvgCircle(circle1);
+            circle2.CenterY = 60;
+
+            var circle3 = new SvgCircle(circle1);
+            circle3.CenterY = 90;
+
+            svg.Add(circle1);
+            svg.Add(circle2);
+            svg.Add(circle3);
         }
     }
 }
